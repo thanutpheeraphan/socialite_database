be125325c3bf88b9a1d3a4edd1642e97e9179681
Thanut Pheeraphan <thanut@wongnai.com>
2022-05-13 13:36:14 +0700

    revamp database model

--- server/database.sql
+++ server/database.sql
@@ -1,24 +1,78 @@
 CREATE DATABASE socialite_db;
 
---set extention
+--set extension
 CREATE TABLE users(
-	user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+	user_id uuid DEFAULT uuid_generate_v4(),
 	user_name VARCHAR(255) NOT NULL,
 	user_email VARCHAR(255) NOT NULL,
-	user_password VARCHAR(255) NOT NULL
+	user_password VARCHAR(255) NOT NULL,
+	PRIMARY KEY(customer_id)
 );
 
 CREATE TABLE rooms(
 	user_id uuid DEFAULT uuid_generate_v4(),
-	room_name VARCHAR(255) PRIMARY KEY NOT NULL,
-	room_link  INTEGER NOT NULL,
+	room_name VARCHAR(255) UNIQUE NOT NULL,
+	room_link  uuid DEFAULT uuid_generate_v4() NOT NULL,  
 	room_member INTEGER NOT NULL,
-	tags VARCHAR(255)[],
 	password VARCHAR(255), 
-	status BOOLEAN
+	status BOOLEAN,
+	PRIMARY KEY (room_link)
 );
+
+create table room_tags
+(
+    room_link uuid DEFAULT uuid_generate_v4(),  
+	CONSTRAINT fk_room
+      FOREIGN KEY(room_link) 
+	  REFERENCES rooms(room_link)
+	  ON DELETE CASCADE,
+    tag_name VARCHAR(255) not null,  
+	CONSTRAINT fk_tag
+      FOREIGN KEY(tag_name) 
+	  REFERENCES tags(tag_name)
+	  ON DELETE CASCADE,
+	PRIMARY KEY (room_link, tag_name)
+);
+
+CREATE TABLE tags(
+	tag_id    SERIAL, 
+	tag_name  VARCHAR(255) NOT NULL, 
+	PRIMARY KEY(tag_name)
+);
+
+ALTER TABLE tags 
+ALTER COLUMN tag_id TYPE SERIAL;
+
+ALTER TABLE rooms ADD PRIMARY KEY (room_name);
+
+ALTER TABLE rooms
+  ADD CONSTRAINT room_name 
+    PRIMARY KEY (order_detail_id);
+
+ALTER TABLE rooms
+  DROP COLUMN tags;
 
 
 
 --insert fake users
 INSERT INTO users (user_name,user_email,user_password) VALUES ('henry','henryhsiesh@gmail.com','helloworld123');
+
+
+--------------------------------------------------------------------------------------------
+create table rooms
+(
+   room_id    integer primary key not null,
+   username   varchar(100) not null
+);
+
+create table tags
+(
+   tag_id    integer primary key not null,
+   tag_name  varchar(100) not null
+);
+
+create table room_tags
+(
+    room_id   integer not null references rooms(room_id),
+    tag_id   integer not null references tags( tag_id )
+); 
--- server/routes/rooms.js
+++ server/routes/rooms.js
@@ -4,106 +4,176 @@ const pool = require("../db");
 
 router.post("/createroom", async (req, res) => {
   try {
-    const { user_id, room_name, password, room_link, status, room_member } =
-      req.body;
+    const {
+      user_id,
+      room_name,
+      password,
+      room_link,
+      status,
+      room_member,
+      tags, //tags={fun,games}
+    } = req.body;
+
+	let checkList = "(";
+	for (var i = 0; i < tags.length; i++) {
+		checkList += "'" + tags[i] + "',";
+    }
+	checkList = checkList.slice(0, -1);
+	checkList += ")";
+	let testList = "";
+	for (var i = 0; i < tags.length; i++) {
+		testList += "('" + tags[i] + "'),";
+    }
+	testList = testList.slice(0, -1);
+	// console.log(testList);
+	// console.log(checkList)
 
     let newRoom = await pool.query(
-      "INSERT INTO rooms (user_id,room_name,password,room_link,status,room_member) VALUES ($1, $2 ,$3,$4,$5,$6) RETURNING * ",
+
+		// `WITH e AS(
+		// 	INSERT INTO rooms (user_id,room_name,password,room_link,status,room_member) 
+		// 		   VALUES ($1, $2 ,$3,$4,$5,$6) 
+		// 	ON CONFLICT(room_name) DO NOTHING
+		// 	RETURNING room_link
+		// )
+		// SELECT * FROM e
+		// UNION
+		// select room_link from rooms where (room_link = $4);`,[user_id, room_name, password, room_link, status, room_member]
+
+      "INSERT INTO rooms (user_id,room_name,password,room_link,status,room_member) VALUES ($1, $2 ,$3,$4,$5,$6) RETURNING room_link",
       [user_id, room_name, password, room_link, status, room_member]
     );
 
-	console.log("Room created");
-    return res.status(200).json(newRoom.rows[0]);
+    let newTags = await pool.query(
+		// `INSERT INTO tags (tag_name) VALUES ${testList} ON CONFLICT DO NOTHING`
+		`WITH e AS(
+			INSERT INTO tags (tag_name) 
+				   VALUES  ${testList}
+			ON CONFLICT(tag_name) DO NOTHING
+			RETURNING tag_name
+		)
+		SELECT * FROM e
+		UNION
+		select tag_name from tags where tag_name in ${checkList};`
+    );
+
+	let newTagList="";
+	// let newRoom = "9940fbe1-092e-414c-b000-af4e77dbcb88";
+	newRoom = newRoom.rows[0].room_link;
+	for (let j = 0; j < newTags.rowCount; j++) {
+		newTagList += "('"+newRoom+"','"+ newTags.rows[j].tag_name + "'),";
+    }
+	newTagList = newTagList.slice(0, -1);
+	console.log("What the hell: ", newTagList);
+	// console.log(testList);
+	console.log(checkList);
+
+    let roomTag = await pool.query(
+		// `WITH e AS(
+		// 	INSERT INTO room_tags (room_link , tag_name)
+		// 		   VALUES  ${newTagList} 
+		// 	ON CONFLICT(room_link) DO NOTHING
+		// 	RETURNING room_tags_id
+		// )
+		// SELECT * FROM e
+		// UNION
+		// select room_tags_id from room_tags where (room_link = $1 and tag_name in ${checkList});`,[newRoom]
+	`INSERT INTO room_tags (room_link , tag_name) VALUES ${newTagList} ON CONFLICT DO NOTHING RETURNING *`
+
+    );
+		
+    
+    return res.status(200).json(roomTag.rows);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server Error");
+    res.send(err);
   }
 });
 
 router.get("/getrooms", async (req, res) => {
-	
   try {
-    const roomsfromdb = pool.query( "SELECT * FROM rooms ORDER BY room_name ASC");
-	res.status(200).json((await roomsfromdb).rows);
-
+    const roomsfromdb = pool.query(
+      "SELECT * FROM rooms ORDER BY room_name ASC"
+    );
+    res.status(200).json((await roomsfromdb).rows);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
 
-router.get("/noOfmembers/:room_link",async(req,res) =>{
+router.get("/noOfmembers/:room_link", async (req, res) => {
+  try {
+    const { room_link } = req.params;
+    const roomsfromdb = pool.query(
+      "SELECT * FROM rooms WHERE (room_link = $1 AND status = $2)",
+      [room_link, true]
+    );
+    res.status(200).json((await roomsfromdb).rows[0].room_member);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
-	try {
-		const {room_link} = req.params;
-		const roomsfromdb = pool.query( "SELECT * FROM rooms WHERE (room_link = $1 AND status = $2)", [room_link,true]);
-		res.status(200).json((await roomsfromdb).rows[0].room_member);
-		
-	} catch (err) {
-		console.error(err.message);
-		res.status(500).send("Server Error");
-	}
-})
+router.put("/userjoined", async (req, res) => {
+  try {
+    const { room_name, room_link, user_state } = req.body;
 
-router.put("/userjoined", async (req,res)=>{
-	try {
-		const {room_name, room_link , user_state} =
-		req.body;
+    console.log("room_link ", room_link);
+    const roomsfromdb = await pool.query(
+      "SELECT * FROM rooms WHERE (room_link = $1 AND status = $2)",
+      [room_link, true]
+    );
+    console.log(roomsfromdb.rows);
+    let noOfMembers = roomsfromdb.rows[0].room_member;
 
-		console.log("room_link ", room_link);
-		const roomsfromdb = await pool.query( "SELECT * FROM rooms WHERE (room_link = $1 AND status = $2)", [room_link,true]);
-		console.log(roomsfromdb.rows)
-		let noOfMembers= roomsfromdb.rows[0].room_member;
-		
+    noOfMembers += 1;
+    const temp = pool.query(
+      "UPDATE rooms SET room_member = $1 WHERE (room_link= $2)",
+      [noOfMembers, room_link]
+    );
 
+    res.status(200).json(roomsfromdb.rows[0]);
+    // res.status(200).json((await roomsfromdb).rows[0].room_member);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
-		noOfMembers += 1;
-		const temp = pool.query(
-			'UPDATE rooms SET room_member = $1 WHERE (room_link= $2)', [noOfMembers,room_link]);
+router.delete("/close/:room_link", async (req, res) => {
+  try {
+    // let {room_name, room_link } =
+    // req.body;
+    const { room_link } = req.params;
+    // DELETE FROM position WHERE unit != ($1) AND user_id = $2
+    const deleteRoom = await pool.query(
+      "DELETE FROM rooms WHERE (room_link = $1)",
+      [room_link]
+    );
+    res.json("Room was deleted");
+    // console.log(listofRooms);
+    // return res.json("Room Deleted");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
-		res.status(200).json((roomsfromdb).rows[0]);
-		// res.status(200).json((await roomsfromdb).rows[0].room_member);
-	} catch (err) {
-		console.error(err.message);
-		res.status(500).send("Server Error");
-	}
+router.get("/searchroom", async (req, res) => {
+  try {
+    const { name } = req.query;
 
-})
+    const rooms = await pool.query(
+      "SELECT * FROM rooms WHERE room_name ILIKE $1 ORDER BY room_name ASC",
+      [`%${name}%`]
+    );
 
-router.delete("/close/:room_link",async(req,res)=>{
-	try {
-		// let {room_name, room_link } =
-		// req.body;
-		const {room_link} = req.params;
-		// DELETE FROM position WHERE unit != ($1) AND user_id = $2
-		const deleteRoom = await pool.query( "DELETE FROM rooms WHERE (room_link = $1)", [room_link]);
-		res.json("Room was deleted")
-		// console.log(listofRooms);
-		// return res.json("Room Deleted");
-
-	} catch (err) {
-		console.error(err.message);
-		res.status(500).send("Server Error");
-	}
-})
-
-router.get("/searchroom",async(req,res)=>{
-	try {
-		const {name} = req.query;
-
-		const rooms = await pool.query("SELECT * FROM rooms WHERE room_name ILIKE $1 ORDER BY room_name ASC",[`%${name}%`]);
-
-		res.json(rooms.rows);
-		
-	} catch (err) {
-		console.error(err.message);
-		
-	}
-})
-
-
-
-
-
+    res.json(rooms.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 module.exports = router;
